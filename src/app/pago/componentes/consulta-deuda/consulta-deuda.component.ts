3716f080e1db13f030d93a16ec213c8831dac2a2
Jesid Polo <jesing482@gmail.com>
2023-01-31 10:53:02 -0500

    ajustando formularios consulta pago

--- src/app/pago/componentes/consulta-deuda/consulta-deuda.component.ts
+++ src/app/pago/componentes/consulta-deuda/consulta-deuda.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { WompiService } from '../../servicios/wompi.service';
+import { ReCaptcha2Component } from 'ngx-captcha';
 
 @Component({
   selector: 'app-modal-consulta-deuda',
@@ -11,9 +12,11 @@ import { WompiService } from '../../servicios/wompi.service';
   styleUrls: ['./consulta-deuda.component.css']
 })
 export class ConsultaDeudaComponent implements OnInit {
+  @ViewChild('captchaElem') captcha!: ReCaptcha2Component
   public llaveCaptcha = '6LemnwgjAAAAAD4NV9ROf1inZOsO5tmM71nNfaQn'
   public formulario: FormGroup
   public deuda: boolean = false;
+  public consultado: boolean = false;
   constructor(private servicioWompi: WompiService, private enrutador: Router) {
     this.formulario = new FormGroup({
       tipo_documento: new FormControl('', [Validators.required]),
@@ -25,38 +28,41 @@ export class ConsultaDeudaComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public consultarDueda(): void{
+  public consultarDueda(): void {
+    console.log('consultando deudas')
     if (this.formulario.invalid) {
+      console.log(this.formulario)
       this.marcarFormularioComoSucio();
+      return;
     }
-    if (this.formulario.valid) {
-      this.servicioWompi.consultarDeuda(
-        (this.formulario.controls['numero_documento'].value).toString(),
-        this.formulario.controls['tipo_documento'].value
-      ).subscribe((respuesta) => {
-        this.deuda = respuesta.deuda
-      }), (error: HttpErrorResponse) => {
-        console.log(error)
-        }
+    this.servicioWompi.consultarDeuda(
+      (this.formulario.controls['numero_documento'].value).toString(),
+      this.formulario.controls['tipo_documento'].value
+    ).subscribe((respuesta) => {
+      this.consultado = true
+      this.deuda = respuesta.deuda
+    }), (error: HttpErrorResponse) => {
+      console.log(error)
     }
-}
 
-  public consultarOtraDueda(): void{
-    this.deuda = false
+  }
+
+  public consultarOtraDueda(): void {
+    this.consultado = false
     this.limpiarFormulario();
-}
+  }
 
-public procesoDePago(): void{
-  let parametros: any[] = [];
-  parametros.push((this.formulario.controls['numero_documento'].value));
-  parametros.push(this.formulario.controls['tipo_documento'].value);
-  //this.enrutador.navigate(['/proceso-pago'], { queryParams: { formulario: parametros } })
-  window.open(`/wordpress/zona-pagos?numero_documento=${this.formulario.controls['numero_documento'].value}&tipo_documento=${this.formulario.controls['tipo_documento'].value}`, '_parent');
-  this.limpiarFormulario();
-}
+  public procesoDePago(): void {
+    let parametros: any[] = [];
+    parametros.push((this.formulario.controls['numero_documento'].value));
+    parametros.push(this.formulario.controls['tipo_documento'].value);
+    //this.enrutador.navigate(['/proceso-pago'], { queryParams: { formulario: parametros } })
+    window.open(`/wordpress/zona-pagos?numero_documento=${this.formulario.controls['numero_documento'].value}&tipo_documento=${this.formulario.controls['tipo_documento'].value}`, '_parent');
+    this.limpiarFormulario();
+  }
 
-  public marcarFormularioComoSucio():void{
-    (<any>Object).values(this.formulario.controls).forEach((control:FormControl) => {
+  public marcarFormularioComoSucio(): void {
+    (<any>Object).values(this.formulario.controls).forEach((control: FormControl) => {
       control.markAsDirty();
       if (control) {
         control.markAsDirty()
@@ -64,8 +70,9 @@ public procesoDePago(): void{
     });
   }
 
-  public limpiarFormulario(){
+  public limpiarFormulario() {
     this.formulario.reset()
+    this.captcha.resetCaptcha()
   }
 
 }
