f687063f113138c210629210b3154bb66b4863d8
Jesid Polo <jesing482@gmail.com>
2023-05-24 10:35:19 -0500

    agregando columna tipo de carga, mes y anio a carga

--- src/app/reportes/reportes.module.ts
+++ src/app/reportes/reportes.module.ts
@@ -4,15 +4,25 @@ import { PaginaReportesComponent } from './componentes/pagina-reportes/pagina-re
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GraficoBarrasComponent } from './componentes/grafico-barras/grafico-barras.component';
+import { RouterModule } from '@angular/router';
+import { PaginaReporteColocacionComponent } from './componentes/tipos-reportes/pagina-reporte-colocacion/pagina-reporte-colocacion.component';
+import { PaginaReporteSaldosCarteraComponent } from './componentes/tipos-reportes/pagina-reporte-saldos-cartera/pagina-reporte-saldos-cartera.component';
+import { PaginaReporteOperacionesComponent } from './componentes/tipos-reportes/pagina-reporte-operaciones/pagina-reporte-operaciones.component';
+import { FiltrosColocacionComponent } from './componentes/filtros/filtros-colocacion/filtros-colocacion.component';
 
 
 
 @NgModule({
   declarations: [
     PaginaReportesComponent,
-    GraficoBarrasComponent
+    GraficoBarrasComponent,
+    PaginaReporteColocacionComponent,
+    PaginaReporteSaldosCarteraComponent,
+    PaginaReporteOperacionesComponent,
+    FiltrosColocacionComponent
   ],
   imports: [
+    RouterModule,
     CommonModule,
     NgbModule,
     FormsModule,
